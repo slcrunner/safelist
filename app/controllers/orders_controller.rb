8c5aa1888d8c3bf131e491427fe8abde61a06fa8
Javier Cortez <apps@devstarlab.com>
2016-03-25 00:13:11 -0600

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,22 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  respond_to :html
-
-  def index
-    @orders = Order.all
-    respond_with(@orders)
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
 
-  def show
-    respond_with(@order)
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
+  # GET /orders/new
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
-    respond_with(@order)
   end
 
-  def edit
-  end
-
+  # POST /orders
+  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @listing = Listing.find(params[:listing_id])
@@ -34,26 +30,22 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_url, notice: 'Order was successfully created.' }
-      end
-
-      def update
-        @order.update(order_params)
-        respond_with(@order)
-      end
-
-      def destroy
-        @order.destroy
-        respond_with(@order)
-      end
-
-      private
-      def set_order
-        @order = Order.find(params[:id])
-      end
-
-      def order_params
-        params.require(:order).permit(:address, :city, :state)
+        format.json { render action: 'show', status: :created, location: @order }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
-end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:address, :city, :state)
+    end
+  end
